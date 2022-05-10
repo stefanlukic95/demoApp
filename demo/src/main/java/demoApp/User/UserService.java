863357ae94cb24c7f2731689d2346828e3495532
Stefan Lukic <stefan.lukic@yahoo.com>
2022-05-10 10:19:21 +0200

    com

--- demo/src/main/java/demoApp/User/UserService.java
+++ demo/src/main/java/demoApp/User/UserService.java
@@ -1,16 +1,13 @@
 package demoApp.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
